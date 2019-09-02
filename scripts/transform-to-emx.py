e238348694c7c4ea9a0ffbf17ca9d81fe84d21e8
connor <c.stroomberg@gmail.com>
2019-09-02 15:52:54 +0200

    feat: read all non repeating tables

--- scripts/transform-to-emx.py
+++ scripts/transform-to-emx.py
@@ -16,19 +16,21 @@ print('--- load data ' + fileName + ' ---')
 header = headerService.readOpenClinHeaderData(filePath)
 headerService.printHeaderData(header)
 
-
 df = pd.read_csv(filePath, sep='\\t', engine='python', skiprows=header.nHeaderRows, converters={i: str for i in range(1182)})
 print(df.info())
 
 # Remove white space from column headers 
 df.rename(columns=lambda x: x.strip(), inplace=True)
 
-dfE1C6 = tableService.transformTable(df, 'E1_C6')
-# print(dfE1C6.info())
+# Use header data to create separate out files for each case report form
+for studyEvent in header.studyEvents:
+                print('   ' + studyEvent.name + ' : ' + studyEvent.key)
+                for crf in studyEvent.caseReportFroms:
+                        print('       ' + crf.name + ' : ' + crf.version + ' : ' + crf.key)
+                        outTable = tableService.transformTable(df, studyEvent.key + '_' + crf.key)
+                        molgenisTableName = crf.name
+                        outFile = outPutFolder + molgenisGroupPrefix + molgenisTableName + '.csv'
+                        outTable.to_csv(outFile, index=False, header=True)
 
-molgenisTableName = 'inclusion'
-outFile = outPutFolder + molgenisGroupPrefix + molgenisTableName + '.csv'
 
-dfE1C6.to_csv(outFile, index=False, header=True)
-
-print('--- all done ---')
+print('--- All done, output at: ' + outPutFolder + ' ---')
